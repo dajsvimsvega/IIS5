39bd2ddd31a1afd616e653858e592e98a91eebab
markobojkic <marko.bojkic@uns.ac.rs>
2019-12-17 15:30:08 +0100

    Vezbe 10

---
+++ IIS5/src/gui/DlgIgrac.java
@@ -0,0 +1,126 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class DlgIgrac extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtIme;
+	private JTextField txtPrezime;
+	public boolean isOk;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			DlgIgrac dialog = new DlgIgrac();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public DlgIgrac() {
+		setBounds(100, 100, 450, 228);
+		setTitle("Unos/Modifikacija igraca");
+		setResizable(false);
+		setModal(true);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		JLabel lblImeIgraca = new JLabel("Ime igraca:");
+		txtIme = new JTextField();
+		txtIme.setColumns(10);
+		JLabel lblPrezimeIgraca = new JLabel("Prezime igraca:");
+		txtPrezime = new JTextField();
+		txtPrezime.setColumns(10);
+		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
+		gl_contentPanel.setHorizontalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addGap(177)
+							.addComponent(lblImeIgraca))
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addGap(161)
+							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPanel.createSequentialGroup()
+									.addGap(10)
+									.addComponent(lblPrezimeIgraca))
+								.addComponent(txtIme, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtPrezime, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+					.addContainerGap(177, Short.MAX_VALUE))
+		);
+		gl_contentPanel.setVerticalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblImeIgraca)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtIme, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblPrezimeIgraca)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtPrezime, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(121, Short.MAX_VALUE))
+		);
+		contentPanel.setLayout(gl_contentPanel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton btnPotvrdi = new JButton("Potvrdi");
+				btnPotvrdi.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if (txtIme.getText().trim().isEmpty() ||
+								txtPrezime.getText().trim().isEmpty()) {
+							isOk = false;
+							setVisible(true);
+							getToolkit().beep();
+							JOptionPane.showMessageDialog(null,
+									"Sva polja moraju biti popunjena!");
+						} else {
+							isOk = true;
+							dispose();
+						}
+					}
+				});
+				btnPotvrdi.setActionCommand("OK");
+				buttonPane.add(btnPotvrdi);
+				getRootPane().setDefaultButton(btnPotvrdi);
+			}
+			{
+				JButton btnOdustani = new JButton("Odustani");
+				btnOdustani.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				btnOdustani.setActionCommand("Cancel");
+				buttonPane.add(btnOdustani);
+			}
+		}
+	}
+
+}
---
+++ IIS5/src/gui/FrmIgraci.java
@@ -0,0 +1,269 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JToggleButton;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import javax.swing.JTextField;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JComboBox;
+
+public class FrmIgraci extends JFrame {
+
+	private JPanel contentPane;
+	private final ButtonGroup btnGroup = new ButtonGroup();
+	JList listIgraci = new JList();
+	private DefaultListModel<String> dlm = new DefaultListModel<String>();
+	private JTextField txtIgrac;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmIgraci frame = new FrmIgraci();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmIgraci() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 682, 395);
+		setResizable(false);
+		setTitle("Frame Igraci");
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel pnlSouth = new JPanel();
+		contentPane.add(pnlSouth, BorderLayout.SOUTH);
+		
+		JButton btnIspisi = new JButton("Ispi\u0161i");
+		btnIspisi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Test Button");
+			}
+		});
+		pnlSouth.add(btnIspisi);
+		
+		JPanel pnlCenter = new JPanel();
+		contentPane.add(pnlCenter, BorderLayout.CENTER);
+		
+		JLabel lblAleksandarKolarov = new JLabel("Aleksandar Kolarov");
+		
+		JLabel lblNemanjaMatic = new JLabel("Nemanja Matic");
+		
+		JLabel lblDusanTadic = new JLabel("Dusan Tadic");
+		
+		JToggleButton tglbtnKolarov = new JToggleButton("Kolarov");
+		tglbtnKolarov.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if (tglbtnKolarov.isSelected()) {
+					dlm.addElement(lblAleksandarKolarov.getText());
+					// obavezno java awt
+					lblAleksandarKolarov.setForeground(Color.BLUE);
+					lblDusanTadic.setForeground(Color.RED);
+					lblNemanjaMatic.setForeground(Color.RED);
+				}
+			}
+		});
+		
+		JToggleButton tglbtnMatic = new JToggleButton("Matic");
+		tglbtnMatic.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if (tglbtnMatic.isSelected()) {
+					dlm.addElement(lblNemanjaMatic.getText());
+					lblAleksandarKolarov.setForeground(Color.RED);
+					lblDusanTadic.setForeground(Color.RED);
+					lblNemanjaMatic.setForeground(Color.BLUE);
+				}
+			}
+		});
+		
+		JToggleButton tglbtnTadic = new JToggleButton("Tadic");
+		tglbtnTadic.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if (tglbtnTadic.isSelected()) {
+					dlm.addElement(lblDusanTadic.getText());
+					lblAleksandarKolarov.setForeground(Color.RED);
+					lblDusanTadic.setForeground(Color.BLUE);
+					lblNemanjaMatic.setForeground(Color.RED);
+				}
+			}
+		});
+		
+		// dodavanje tgl-btn-a u ButtonGroup
+		btnGroup.add(tglbtnKolarov);
+		btnGroup.add(tglbtnMatic);
+		btnGroup.add(tglbtnTadic);
+		
+		JScrollPane scrlPaneIgraci = new JScrollPane();
+		
+		JLabel lblUnesiIgraca = new JLabel("Unesi igraca:");
+		
+		txtIgrac = new JTextField();
+		txtIgrac.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER){
+					dlm.addElement(txtIgrac.getText()); 
+				}
+			}
+		});
+		txtIgrac.setColumns(10);
+		
+		JLabel lblOdaberiIgraca = new JLabel("Odaberi igraca:");
+		
+		JComboBox cbxIgraci = new JComboBox();
+		
+		cbxIgraci.addItem("Luka Jovic");
+		cbxIgraci.addItem("Dusan Vlahovic");
+		cbxIgraci.addItem("Nemanja Gudelj");
+		
+		JButton btnDodajIgraca = new JButton("Dodaj igraca");
+		btnDodajIgraca.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dlm.addElement((String) cbxIgraci.getSelectedItem());
+			}
+		});
+		
+		JButton btnUkloni = new JButton("Ukloni");
+		btnUkloni.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!listIgraci.isSelectionEmpty()) {
+					dlm.removeElement(listIgraci.getSelectedValue());
+				} else {
+					JOptionPane.showMessageDialog(null,
+							"Niste selektovali nijednog igraca!");
+				}
+			}
+		});
+		
+		JButton btnDijalog = new JButton("Dijalog");
+		btnDijalog.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// ovde smo stali
+			}
+		});
+		
+		JButton btnModifikacijaDijalog = new JButton("Modifikacija dijalog");
+		
+		GroupLayout gl_pnlCenter = new GroupLayout(pnlCenter);
+		gl_pnlCenter.setHorizontalGroup(
+			gl_pnlCenter.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pnlCenter.createSequentialGroup()
+					.addGroup(gl_pnlCenter.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(gl_pnlCenter.createSequentialGroup()
+							.addGap(30)
+							.addGroup(gl_pnlCenter.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(lblUnesiIgraca, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(tglbtnTadic, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(tglbtnMatic, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(tglbtnKolarov, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtIgrac))
+						.addGroup(gl_pnlCenter.createSequentialGroup()
+							.addGap(21)
+							.addGroup(gl_pnlCenter.createParallelGroup(Alignment.LEADING)
+								.addComponent(btnDodajIgraca)
+								.addGroup(gl_pnlCenter.createSequentialGroup()
+									.addComponent(lblOdaberiIgraca, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
+									.addGap(18)
+									.addComponent(cbxIgraci, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))))
+					.addGroup(gl_pnlCenter.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_pnlCenter.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_pnlCenter.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(lblAleksandarKolarov, GroupLayout.DEFAULT_SIZE, 126, Short.MAX_VALUE)
+								.addComponent(lblNemanjaMatic, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblDusanTadic, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(scrlPaneIgraci, GroupLayout.PREFERRED_SIZE, 258, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap(32, Short.MAX_VALUE))
+						.addGroup(gl_pnlCenter.createSequentialGroup()
+							.addGap(103)
+							.addGroup(gl_pnlCenter.createParallelGroup(Alignment.LEADING)
+								.addComponent(btnDijalog, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnModifikacijaDijalog))
+							.addGap(208))))
+				.addGroup(Alignment.TRAILING, gl_pnlCenter.createSequentialGroup()
+					.addContainerGap(556, Short.MAX_VALUE)
+					.addComponent(btnUkloni)
+					.addGap(73))
+		);
+		gl_pnlCenter.setVerticalGroup(
+			gl_pnlCenter.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pnlCenter.createSequentialGroup()
+					.addGap(14)
+					.addGroup(gl_pnlCenter.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_pnlCenter.createSequentialGroup()
+							.addGroup(gl_pnlCenter.createParallelGroup(Alignment.BASELINE)
+								.addComponent(tglbtnKolarov)
+								.addComponent(lblAleksandarKolarov))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_pnlCenter.createParallelGroup(Alignment.BASELINE)
+								.addComponent(tglbtnMatic)
+								.addComponent(lblNemanjaMatic))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_pnlCenter.createParallelGroup(Alignment.BASELINE)
+								.addComponent(tglbtnTadic)
+								.addComponent(lblDusanTadic))
+							.addGap(16))
+						.addGroup(gl_pnlCenter.createSequentialGroup()
+							.addComponent(scrlPaneIgraci, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnUkloni)
+					.addGap(16)
+					.addGroup(gl_pnlCenter.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblUnesiIgraca)
+						.addComponent(txtIgrac, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnDijalog, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
+					.addGap(21)
+					.addGroup(gl_pnlCenter.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblOdaberiIgraca)
+						.addComponent(cbxIgraci, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnModifikacijaDijalog))
+					.addGap(26)
+					.addComponent(btnDodajIgraca)
+					.addContainerGap(42, Short.MAX_VALUE))
+		);
+		
+		// povezivanje modela i liste
+		listIgraci.setModel(dlm);
+		
+		scrlPaneIgraci.setViewportView(listIgraci);
+		pnlCenter.setLayout(gl_pnlCenter);
+	}
+}
